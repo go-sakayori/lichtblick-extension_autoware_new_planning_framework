f1d0d1a59b9f9699994be1f3507fe4920f5d051f
Go Sakayori <go-sakayori@users.noreply.github.com>
2024-12-20 13:56:14 +0900

    feat: use sceneUpdate (#2)
    * make base functions
    
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>
    
    * change origin
    
    Signed-off-by: Go Sakayori <gsakayori@gmail.com>
    
    * change loop start for trajectory point
    
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>
    
    ---------
    
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>
    Signed-off-by: Go Sakayori <gsakayori@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,80 @@
-import { PosesInFrame, Pose } from "@foxglove/schemas";
+import {
+  SceneUpdate,
+  LinePrimitive,
+  LineType,
+  Point3,
+} from "@foxglove/schemas";
 import { Trajectories } from "./Trajectories";
+import { TrajectoryPoint } from "./TrajectoryPoint";
 import { ExtensionContext } from "@lichtblick/suite";
 
-function convertTrajectories(msg: Trajectories): PosesInFrame {
-  const { trajectories } = msg;
-
-  const frame_id = trajectories[0]?.header.frame_id || "";
-  const timestamp = trajectories[0]?.header.stamp || { sec: 0, nsec: 0 };
-
-  // `trajectories` の各データを解構
-  const poses: Pose[] = trajectories.flatMap((trajectory) => {
-    const { points } = trajectory;
-
-    // 各 `point` を `pose` に変換
-    return points.map((point) => ({
-      position: {
-        x: point.pose.position.x,
-        y: point.pose.position.y,
-        z: point.pose.position.z,
-      },
-      orientation: {
-        x: point.pose.orientation.x,
-        y: point.pose.orientation.y,
-        z: point.pose.orientation.z,
-        w: point.pose.orientation.w,
-      },
-    }));
-  });
+function createLinePrimitive(msg: TrajectoryPoint[]): LinePrimitive {
+  const points: Point3[] = [];
+  for (let i = 0; i < msg.length; i++) {
+    points.push({
+      x: msg[i]?.pose.position.x || 0.0,
+      y: msg[i]?.pose.position.y || 0.0,
+      z: msg[i]?.pose.position.z || 0.0,
+    });
+  }
 
   return {
-    frame_id,
-    timestamp,
-    poses,
+    type: LineType.LINE_STRIP,
+    pose: {
+      position: {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+      },
+      orientation: { x: 0, y: 0, z: 0, w: 1 },
+    },
+    thickness: 0.1,
+    scale_invariant: false,
+    points,
+    color: { r: 1, g: 0, b: 1, a: 1 },
+    colors: [],
+    indices: [],
+  };
+}
+
+function convertTrajectories(msg: Trajectories): SceneUpdate {
+  const { trajectories } = msg;
+  const linePrimitives: LinePrimitive[] = [];
+
+  for (let i = 0; i < (trajectories.length || 0); i++) {
+    const trajectory = trajectories[i];
+    if (!trajectory || !trajectory.points) continue;
+    const linePrimitive: LinePrimitive = createLinePrimitive(trajectory.points);
+    linePrimitives.push(linePrimitive);
+  }
+
+  return {
+    deletions: [],
+    entities: [
+      {
+        timestamp: trajectories[0]?.header.stamp || { sec: 0, nsec: 0 },
+        frame_id: trajectories[0]?.header.frame_id || "",
+        id: "generated_paths",
+        lifetime: { sec: 1.0, nsec: 0 },
+        frame_locked: false,
+        metadata: [],
+        arrows: [],
+        cubes: [],
+        spheres: [],
+        cylinders: [],
+        lines: linePrimitives,
+        triangles: [],
+        texts: [],
+        models: [],
+      },
+    ],
   };
 }
 
 export function activate(extensionContext: ExtensionContext): void {
   extensionContext.registerMessageConverter({
     fromSchemaName: "autoware_new_planning_msgs/msg/Trajectories",
-    toSchemaName: "foxglove.PosesInFrame",
+    toSchemaName: "foxglove.SceneUpdate",
     converter: convertTrajectories,
   });
 }
